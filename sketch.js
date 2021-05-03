f41443db93a59b414f009e3fefdfae19fcfe30ae
sarademing <77241999+sarademing@users.noreply.github.com>
2021-05-03 12:19:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function draw() {
   background(255, 204, 0);
   rect(0,0,700, 500);
   image(img1, 0, 0);
-  image(img2, 0, 100);
+  image(img2, 100, 200);
   image(img3, 1500, 100);
   image(img4, 500, 300);
   //think of this area as a looping function that continuall puts things on screen, in order
