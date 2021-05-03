140ac6efb60095e21e45d8012aa32d165add0439
sarademing <77241999+sarademing@users.noreply.github.com>
2021-05-03 10:45:47 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,19 +3,18 @@ let img1;
 let img2;
 let img3;
 let img4;
-let sound;
-let vid1;
+//let vid1;
 function preload(){
   img1 = loadImage('assets/pond.jpg');
   img2 = loadImage('assets/waterFall.jpg');
   img3 = loadImage('assets/tree.jpg');
   img4 = loadImage('assets/lake.jpg');
-  vid1 = createVideo('assets/waterFall2.MOV');
+  //vid1 = createVideo('assets/waterFall2.MOV');
   //stuff to prepare gets done here
 }
 function setup() {
   createCanvas(1920, 1080);
-  vid1.position(100, 600);
+  //vid1.position(100, 600);
 //things to establish the "sandbox" go here
 }
 function draw() {
@@ -27,13 +26,13 @@ function draw() {
   image(img4, 500, 300);
   //think of this area as a looping function that continuall puts things on screen, in order
 }
-function mousePressed() {
-if(vid1.isPlaying(assets/waterFall2.MOV)) {
-  vid1.stop(assets/waterFall2.MOV);
-}
-else {
-  vid1.play(assets/waterFall2.MOV);
-}
+//function mousePressed() {
+//if(vid1.isPlaying(assets/waterFall2.MOV)) {
+//  vid1.stop(assets/waterFall2.MOV);
+//}
+//else {
+//  vid1.play(assets/waterFall2.MOV);
+//}
 
 
-  }
+ // }
