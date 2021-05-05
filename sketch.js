c832e1b17edc15cc29a9355edd4d5e274bab5c10
sarademing <77241999+sarademing@users.noreply.github.com>
2021-05-05 15:41:15 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function setup() {
 function draw() {
   background(255, 204, 0);
   rect(0,0,750, 400);
-  rect(650,500,750,400);
+  rect(700,500,750,400);
   image(img1, 0, 0);
   image(img2, 250, 0);
   image(img3, 500, 0);
