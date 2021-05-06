1619fd049bc7753988356ea77307893cbc23866a
sarademing <77241999+sarademing@users.noreply.github.com>
2021-05-06 15:15:37 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,6 +30,7 @@ function draw() {
   fill("yellow");
   circle(1130, 200, 80);
   circle(1130, 100, 100);
+  ellipse(1130, 100, 60, 100);
   image(img1, 0, 0);
   image(img2, 250, 0);
   image(img3, 500, 0);
